d6b8d2d60f6a8ebdc9ee43c9baa79e12850cda3f
José Alberto Trejo Serrano <joseatrejos@gmail.com>
2020-04-19 18:37:51 -0700

    Comportamiento del enemigo v1.0

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,38 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : Enemy2D
+public class Enemy : MonoBehaviour
 {
-    public Transform target;
-    public float chaseRadius;
-    public float attackRadius;
-    public Transform homePosition;
-
-    void Start()
-    {
-        target = GameObject.FindWithTag("Player").transform;   
-    }
-
-    void Update()
-    {
-        CheckDistance();
-    }
-
-    void OnCollisionEnter2D(Collision2D col)
-    {
-        if( col.gameObject.tag == "Player" )
-        {
-            Destroy(col.gameObject);
-            Debug.Log("Woof woof");
-        }
-    }
-
-    void CheckDistance()
-    {
-        if( Vector3.Distance(target.position, transform.position) <= chaseRadius
-        && Vector3.Distance(target.position, transform.position) > attackRadius) 
-        {
-            transform.position = Vector3.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
-        }
-    }
+    public int health;
+    public string enemyName;
+    public int baseAttack;
+    public float moveSpeed;
 }
